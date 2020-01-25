db846746c7fe30df28db1ad9d93e5460d1318423
MarvinSkiller <marvinskiller1993@gmail.com>
2020-01-25 14:34:41 +0630

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { CustomMaterialModule } from './core/material/material.module';
+import { ApproutingModule } from './core/approuting/app.routing.module';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
+    ApproutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
